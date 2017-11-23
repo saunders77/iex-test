c1c3fdcb874ae69d63caf01b5d5ca60d81c30a88
Michael Saunders <michaelsaunders@outlook.com>
2017-11-22 17:26:45 -0800

    totally working without cancelation

--- iex-test.js
+++ iex-test.js
@@ -27,42 +27,55 @@ Office.initialize = function(reason){
     var sessionActive = false;
 
     function getPrices(){
+        
         var queryString = "https://api.iextrading.com/1.0/stock/market/batch?symbols=";
-        var fieldString = "";
-        for(ticker in Object.keys(stocks)){
-            queryString += encodeURIComponent(ticker) + "%2C"; // comma
+        for(ticker in stocks){
+            if(stocks.hasOwnProperty(ticker)){
+                queryString += encodeURIComponent(ticker) + "%2C"; // comma
+            }
         }
-        queryString.slice(0,-1); // remove last comma
+        queryString = queryString.slice(0,-3); // remove last comma
         queryString += "&types=quote&filter=";
-        for(field in Object.keys(fields)){
-            fieldString += fieldMapping[field] + "%2C";
+        for(field in fields){
+            if(fields.hasOwnProperty(field)){
+                queryString += fieldMapping[field] + "%2C";
+            }
         }
-        queryString.slice(0, -1);
+        queryString = queryString.slice(0, -3);
+
+        stocks["T"]["last"][0](queryString);
 
         // call the service
         httpGetAsync(queryString, function(data){
             var result = JSON.parse(data);
-            for(ticker in Object.keys(stocks)){
-                for(field in Object.keys(stocks[ticker])){
-                    for(callback in stocks[ticker][field]){
-                        callback(result[ticker]["quote"][fieldMapping[field]]);
+            for(ticker in stocks){
+                if(stocks.hasOwnProperty(ticker)){
+                    for(field in stocks[ticker]){
+                        if(stocks[ticker].hasOwnProperty(field)){
+                            for(var i = 0; i < stocks[ticker][field].length;i++){
+                                stocks[ticker][field][i](result[ticker]["quote"][fieldMapping[field]]);
+                            }
+                        }
                     }
                 }
             }
         });
 
-        setTimeout(getPrices,500);
+        window.setTimeout(function(){
+            getPrices();
+        },500);
     }
 
-    function quote (ticker, field, setResult) {
+    function quote (ticker, field, invocationContext) {
         // add the callback to memory
+        
         if(!stocks[ticker]){
             stocks[ticker] = {};
         }    
         if(!stocks[ticker][field]){
             stocks[ticker][field] = [];
         }
-        stocks[ticker][field].push(setResult);
+        stocks[ticker][field].push(invocationContext.setResult);
         if(!fields[field]){
             fields[field] = true;
         }
@@ -73,6 +86,12 @@ Office.initialize = function(reason){
             sessionActive = true;
             getPrices();
         }
+
+        // remove entry if it's canceled
+        invocationContext.onCanceled = function(){
+            // remove the stock if there are no occurences 
+        };
+
     }   
 
     Excel.Script.CustomFunctions["STOCKS"]["QUOTE"] = {
@@ -80,18 +99,18 @@ Office.initialize = function(reason){
         description: "Get real-time market data from the IEX exchange.",
         helpUrl: "https://www.michael-saunders.com/help.html",
         result: {
-            resultType: Excel.CustomFunctionValueType.number,
+            resultType: Excel.CustomFunctionValueType.string,
             resultDimensionality: Excel.CustomFunctionDimensionality.scalar,
         },
         parameters: [
             {
-                name: "The stock ticker (eg. 'MSFT')",
+                name: 'The stock ticker (eg. "MSFT")',
                 description: "The stock ticker (eg. 'MSFT')",
                 valueType: Excel.CustomFunctionValueType.string,
                 valueDimensionality: Excel.CustomFunctionDimensionality.scalar,
             },
             {
-                name: "The field to query ('last', 'change', 'bid', 'ask', or 'volume')",
+                name: 'The field to query ("last", "change", "bid", "ask", or "volume")',
                 description: "The field to query ('last', 'change', 'bid', 'ask', or 'volume')",
                 valueType: Excel.CustomFunctionValueType.string,
                 valueDimensionality: Excel.CustomFunctionDimensionality.scalar,
@@ -100,39 +119,32 @@ Office.initialize = function(reason){
         options:{ batch: false, stream: true }
     };
     
-    
-
-    // incrementValue is an example of a streaming function.
-    function incrementValue(increment, setResult){    
-    	var result = 0;
-        setInterval(function(){
-            result += increment;
-            setResult(result);
-        }, 1000);
-    }
-    
-    
-    // The refreshTemperature and streamTemperature functions use global variables to save & read state, while streaming data.
-    var savedTemperatures = {};
-    function refreshTemperature(thermometerID){        
-        sendWebRequestExample(thermometerID, function(data){
-            savedTemperatures[thermometerID] = data.temperature;
-        });
-        setTimeout(function(){
-            refreshTemperature(thermometerID);
-        }, 1000);
-    }
-    function streamTemperature(thermometerID, setResult){    
-        if(!savedTemperatures[thermometerID]){
-            refreshTemperature(thermometerID);
-        }
-        function getNextTemperature(){
-            setResult(savedTemperatures[thermometerID]);
-            setTimeout(getNextTemperature, 1000);
-        }
-        getNextTemperature();
-    }
-
+/*
+    Excel.Script.CustomFunctions["STOCKS"]["DEBUG"] = {
+        call: quote,
+        description: "Debugging ",
+        helpUrl: "https://www.michael-saunders.com/help.html",
+        result: {
+            resultType: Excel.CustomFunctionValueType.number,
+            resultDimensionality: Excel.CustomFunctionDimensionality.scalar,
+        },
+        parameters: [
+            {
+                name: 'The stock ticker (eg. "MSFT")',
+                description: "The stock ticker (eg. 'MSFT')",
+                valueType: Excel.CustomFunctionValueType.string,
+                valueDimensionality: Excel.CustomFunctionDimensionality.scalar,
+            },
+            {
+                name: 'The field to query ("last", "change", "bid", "ask", or "volume")',
+                description: "The field to query ('last', 'change', 'bid', 'ask', or 'volume')",
+                valueType: Excel.CustomFunctionValueType.string,
+                valueDimensionality: Excel.CustomFunctionDimensionality.scalar,
+            }
+        ],
+        options:{ batch: false, stream: true }
+    };
+*/
     // Register all the custom functions previously defined in Excel.
     Excel.run(function (context) {        
         context.workbook.customFunctions.addAll();
